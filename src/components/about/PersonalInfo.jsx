bf1b7145bd4719343ec948b51608415c0c403fd4
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-05-02 18:36:21 +0300

    second commit

---
+++ src/components/about/PersonalInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const personalInfoContent = [
+  { meta: "first name", metaInfo: "M. Sertaç" },
+  { meta: "last name", metaInfo: "Toröz" },
+  { meta: "Age", metaInfo: "34 Years" },
+  { meta: "Nationality", metaInfo: "Turkish" },
+  { meta: "Freelance", metaInfo: "Available" },
+  { meta: "Address", metaInfo: "Datça,Mugla,TR" },
+  { meta: "phone", metaInfo: "+905355167003" },
+  { meta: "Email", metaInfo: "msertactoroz@gmail.com" },
+  { meta: "Skype", metaInfo: " sertactoroz" },
+  { meta: "langages", metaInfo: "Turkish, English, German" },
+];
+
+const PersonalInfo = () => {
+  return (
+    <ul className="about-list list-unstyled open-sans-font">
+      {personalInfoContent.map((val, i) => (
+        <li key={i}>
+          <span className="title">{val.meta}: </span>
+          <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+            {val.metaInfo}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PersonalInfo;
