0721e7f52de30f502e1a5e8de7a4fe695892f0d8
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-05-13 17:13:49 +0300

    asd

--- src/components/switch/SwitchLanguage.jsx
+++ src/components/switch/SwitchLanguage.jsx
@@ -5,9 +5,9 @@ const SwitchLanguage = () => {
 
     return (
         <div class="flags pl-3 py-auto">
-            <a href="#" title="Turkish"><span class="flag tr flag-icon flag-icon-tr flag-icon-squared"></span></a>
-            <a href="#" title="English"><span class="flag us flag-icon flag-icon-us flag-icon-squared"></span></a>
-            <a href="#" title="Deutch"><span class="flag de flag-icon flag-icon-de flag-icon-squared"></span></a>
+            <a href="" title="Turkish"><span class="flag tr flag-icon flag-icon-tr flag-icon-squared"></span></a>
+            <a href="" title="English"><span class="flag us flag-icon flag-icon-us flag-icon-squared"></span></a>
+            <a href="" title="Deutch"><span class="flag de flag-icon flag-icon-de flag-icon-squared"></span></a>
         </div>
     );
 };
