399e3f7c9a26631f772d1804d05565212d4b8796
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-05-13 17:13:25 +0300

    language component updated

--- src/components/switch/SwitchLanguage.jsx
+++ src/components/switch/SwitchLanguage.jsx
@@ -4,7 +4,6 @@ import "./SwitchLanguage.css";
 const SwitchLanguage = () => {
 
     return (
-
         <div class="flags pl-3 py-auto">
             <a href="#" title="Turkish"><span class="flag tr flag-icon flag-icon-tr flag-icon-squared"></span></a>
             <a href="#" title="English"><span class="flag us flag-icon flag-icon-us flag-icon-squared"></span></a>
