05183b4f24677cbf94967be14fff6328ed0e9e13
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-05-13 17:18:10 +0300

    asdasd

--- src/components/switch/SwitchLanguage.jsx
+++ src/components/switch/SwitchLanguage.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SwitchLanguage.css";
 
 const SwitchLanguage = () => {
 
     return (
         <div class="flags pl-3 py-auto">
-            <a href="" title="Turkish"><span class="flag tr flag-icon flag-icon-tr flag-icon-squared"></span></a>
-            <a href="" title="English"><span class="flag us flag-icon flag-icon-us flag-icon-squared"></span></a>
-            <a href="" title="Deutch"><span class="flag de flag-icon flag-icon-de flag-icon-squared"></span></a>
+            <a title="Turkish"><span class="flag tr flag-icon flag-icon-tr flag-icon-squared"></span></a>
+            <a title="English"><span class="flag us flag-icon flag-icon-us flag-icon-squared"></span></a>
+            <a title="Deutch"><span class="flag de flag-icon flag-icon-de flag-icon-squared"></span></a>
         </div>
     );
 };
