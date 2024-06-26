341551687f449d1d3a7e5236a54cbbc8376f2cb9
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-06-26 18:47:21 +0300

    scoreboard updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,7 @@ import heroImgMobile from "../../assets/img/hero/img-mobile.jpg";
 import cancelImg from "../../assets/img/cancel.svg";
 import Index from "../../components/about/index";
 import Social from "../../components/Social";
-import ScoreBoard from "../scoreBoard/ScoreBoard";
+import ScoreBoard from "../ScoreBoard";
 import GeoLocation from "../GeoLocation";
 const heroContent = {
   heroImage: heroImg,
@@ -16,18 +16,14 @@ const heroContent = {
   I'm a Turkish full-stack developer dedicated to crafting seamless and user-centric digital experiences. My passion lies in leveraging technology to create innovative solutions that enhance the lives of others.`,
   heroBtn: "more about me",
 };
-
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
   function toggleModalOne() {
     setIsOpen(!isOpen);
   }
-
   return (
     <>
-      <ScoreBoard />
       <div className="row home-details-container align-items-center">
-
         <div
           className="col-lg-4 bg position-fixed d-none d-lg-block"
           style={{
@@ -37,37 +33,29 @@ const Hero = () => {
         ></div>
         <div className="col-12 col-lg-8 offset-lg-4 home-details  text-center text-lg-start">
           <div>
-            <img
+            {/* <img
               src={heroContent.heroMobileImage}
               className="img-fluid main-img-mobile d-sm-block d-lg-none"
               alt="hero man"
-            />
+            /> */}
             <h1 className="text-uppercase poppins-font" style={{ fontSize: '3em' }}>
               I'm {heroContent.heroTitleName}.
               <span style={{ fontSize: '0.8em' }}>{heroContent.heroDesignation}</span>
             </h1>
-
             <p className="open-sans-font">{heroContent.heroDescriptions}</p>
-
             <button className="button" onClick={toggleModalOne}>
               <span className="button-text">{heroContent.heroBtn}</span>
-
               <span className="button-icon fa fa-arrow-right"></span>
             </button>
-
             <div className="contact p-2 pt-4">
               <Social />
             </div>
             <GeoLocation />
-
           </div>
-
+          {/* <ScoreBoard /> */}
         </div>
-
       </div>
-
       {/* End home-details-container */}
-
       {/* Start Modal for About More */}
       <Modal
         isOpen={isOpen}
@@ -78,15 +66,12 @@ const Hero = () => {
         closeTimeoutMS={500}
       >
         <div>
-
           <button className="close-modal" onClick={toggleModalOne}>
             <img src={cancelImg} alt="close icon" />
           </button>
           {/* End close icon */}
-
           <div className="box_inner about">
             <div data-aos="fade-up" data-aos-duration="1200">
-
               <div className="title-section text-left text-sm-center">
                 <h1>
                   ABOUT <span>ME</span>
@@ -104,5 +89,4 @@ const Hero = () => {
     </>
   );
 };
-
 export default Hero;
