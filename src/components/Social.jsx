86fa833eb5ee5b38fff6031ee40b0228c4d2480a
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-08-16 23:11:20 +0300

    a1

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,7 +4,7 @@ const SocialShare = [
   { iconName: "fa fa-instagram", link: "https://www.instagram.com/sertactoroz/" },
   { iconName: "fa fa-facebook", link: "https://www.facebook.com/sertac.toroz/" },
   { iconName: "fa fa-youtube", link: "https://www.youtube.com/@sertactoroz6504" },
-  { iconName: "fa fa-google", link: "https://dribbble.com/" },
+  // { iconName: "fa fa-google", link: "https://dribbble.com/" },
   { iconName: "fa fa-linkedin", link: "https://www.linkedin.com/in/serta%C3%A7tor%C3%B6z/" },
   { iconName: "fa fa-github", link: "https://github.com/sertactoroz" },
   { iconName: "fa fa-xing", link: "https://www.xing.com/profile/MustafaSertac_Toroez/web_profiles" },
